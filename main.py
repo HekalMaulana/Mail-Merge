6a2e72765a9f82912b0ddb4956b4c24fb2eb7a94
Muhammad Hekal Maulana <maulanaahekal@gmail.com>
2023-07-11 02:16:26 +0800

    Making Program for send mail automatic

---
+++ main.py
@@ -0,0 +1,14 @@
+with open("Input/Names/invited_names.txt", "r") as names_letter:
+    list_name_letter = names_letter.readlines()
+
+with open("Input/Letters/starting_letter.txt", "r") as starting_letter:
+    letter = starting_letter.readlines()
+
+first_letter = letter[0].strip()
+
+for name in list_name_letter:
+    name_letter = name.strip()
+    first_letter_change = first_letter.replace("[name],", f"{name_letter},\n\n")
+    with open(f"Output/ReadyToSend/letter_for_{name_letter}", "w") as send:
+        send.write(first_letter_change)
+        send.write("You are invited to my birthday this Friday.\n\nHope you can make it!\n\nHekal.")
